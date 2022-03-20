63d622120cb2a5ec1b87a8ffe63bab60d6e4afa1
zxzx93 <thddustn26@hanmail.net>
2022-03-20 17:03:23 +0900

    cart 페이지 컴포넌트 추가, css 추가

---
+++ pages/cart.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import styles from "../styles/Cart.module.css";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr>
+            <th>product</th>
+            <th>name</th>
+            <th>extras</th>
+            <th>price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+
+          <tr>
+            <td>
+              <div className={styles.imgContainer}>
+                <Image
+                  src="/image/pizza.png"
+                  alt=""
+                  layout="fill"
+                  objectFit="cover"
+                />
+              </div>
+            </td>
+            <td>
+              <span className={styles.name}>coraljo</span>
+            </td>
+            <td>
+              <span className={styles.extras}>
+                Double ingredient, spicy sauce
+              </span>
+            </td>
+            <td>
+              <span className={styles.price}>29000원</span>
+            </td>
+            <td>
+              <span className={styles.quantity}>2</span>
+            </td>
+            <td>
+              <span className={styles.total}>29000원</span>
+            </td>
+          </tr>
+        </table>
+      </div>
+
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>SubTotal : </b>$79.09f
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount : </b>$79.09f
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total : </b>$79.09f
+          </div>
+
+          <button className={styles.button}>Checkout now!</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
